e36fa3e28a5eaeed1542152191016c71cd507c2a
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-04 14:00:32 +0300

    Redirected login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, NavLink , Redirect} from 'react-router-dom';
 
 //stateless component
-const news = () => {
-  return(<h1>News page</h1>)
-}
+const News = ({ match }) => {
+	return(<h1>News page: { match.params.id }</h1>)
+};
+
+
+const Profile = () => {
+	return(<h1>Profile Page</h1>)
+};
 
 class App extends Component {
+
+  state = {
+    loggedIn : false
+  }
+
+	onClickButton = () => {
+	  this.setState(prevState => ({
+      loggedIn: !prevState.loggedIn
+    }))
+  };
+
+
   render() {
     return (
 
       <Router>
         <div>
+
+        <NavLink activeStyle={{ color : "red" }}  exact to="/">-Homepage</NavLink> <br/>
+        <NavLink activeClassName="active-class" exact to="/contact">-Contact</NavLink>   <br/>
+        <NavLink activeStyle={{ color : "red" }}  exact to="/news/2">-News</NavLink>  <br/>
+        <NavLink activeStyle={{ color : "red" }}  exact to="/profile">-Profile</NavLink>  <br/>
+
+        <br/><br/>
+
+        <input
+            type="button"
+            onClick={this.onClickButton}
+            value={ this.state.loggedIn ? 'Logout': 'Login' }
+          />
+        
+
           <Route path="/" exact render={ 
             () => {
 
@@ -22,9 +53,19 @@ class App extends Component {
             }
           } />
 
-          <Route path="/news"  exact strict Component={news} />
-          
+          <Route path="/contact"  exact render={  //strict
+                  () => {
 
+                return(<h1>Contact</h1>)
+            }
+          } />
+
+          <Route path="/news/:id"  exact strict component={News} />
+
+
+          <Route path="/profile" exact strict render={ () => (
+					  this.state.loggedIn ? ( <Profile/>) : (<Redirect to="/" />)
+          )} />
 
         </div>
       </Router>
