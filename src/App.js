ff53e9e27436647c18ae1fff280146dd5bbcfabf
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-04 13:12:17 +0300

    imported BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter } from 'react-router-dom';
+
+
 class App extends Component {
   render() {
     return (
