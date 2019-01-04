1082e229f764c697fa47856cb469fa45c7b15fb6
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-04 14:11:02 +0300

    redirected

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Link, NavLink , Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, NavLink , Redirect, Switch} from 'react-router-dom';
 
 //stateless component
 const News = ({ match }) => {
@@ -12,6 +12,11 @@ const News = ({ match }) => {
 const Profile = () => {
 	return(<h1>Profile Page</h1>)
 };
+
+const Error = () => {
+	return(<h1>404</h1>)
+};
+
 
 class App extends Component {
 
@@ -29,43 +34,49 @@ class App extends Component {
   render() {
     return (
 
-      <Router>
-        <div>
+      
+        <Router>
+          <div>
 
-        <NavLink activeStyle={{ color : "red" }}  exact to="/">-Homepage</NavLink> <br/>
-        <NavLink activeClassName="active-class" exact to="/contact">-Contact</NavLink>   <br/>
-        <NavLink activeStyle={{ color : "red" }}  exact to="/news/2">-News</NavLink>  <br/>
-        <NavLink activeStyle={{ color : "red" }}  exact to="/profile">-Profile</NavLink>  <br/>
+          <NavLink activeStyle={{ color : "red" }}  exact to="/">-Homepage</NavLink> <br/>
+          <NavLink activeClassName="active-class" exact to="/contact">-Contact</NavLink>   <br/>
+          <NavLink activeStyle={{ color : "red" }}  exact to="/news/2">-News</NavLink>  <br/>
+          <NavLink activeStyle={{ color : "red" }}  exact to="/profile">-Profile</NavLink>  <br/>
 
-        <br/><br/>
+          <br/><br/>
 
-        <input
-            type="button"
-            onClick={this.onClickButton}
-            value={ this.state.loggedIn ? 'Logout': 'Login' }
-          />
-        
+          <input
+              type="button"
+              onClick={this.onClickButton}
+              value={ this.state.loggedIn ? 'Logout': 'Login' }
+            />
+          
+          <Switch>
 
-          <Route path="/" exact render={ 
-            () => {
+            <Route path="/" exact render={ 
+              () => {
 
-                return(<h1>Home page</h1>)
-            }
-          } />
+                  return(<h1>Home page</h1>)
+              }
+            } />
 
-          <Route path="/contact"  exact render={  //strict
-                  () => {
+            <Route path="/contact"  exact render={  //strict
+                    () => {
 
-                return(<h1>Contact</h1>)
-            }
-          } />
+                  return(<h1>Contact</h1>)
+              }
+            } />
 
-          <Route path="/news/:id"  exact strict component={News} />
+            <Route path="/news/:id"  exact strict component={News} />
 
 
-          <Route path="/profile" exact strict render={ () => (
-					  this.state.loggedIn ? ( <Profile/>) : (<Redirect to="/" />)
-          )} />
+            <Route path="/profile" exact strict render={ () => (
+              this.state.loggedIn ? ( <Profile/>) : (<Redirect to="/" />)
+            )} />
+
+            <Route component={Error} />
+            
+          </Switch> 
 
         </div>
       </Router>
