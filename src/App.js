032f145e59f3074fcd6c1ed2322d8746a3674700
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-04 13:22:22 +0300

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <Router>
+        <div>
+          <Route path="/" render={ 
+            () => {
+
+                return(<h1>Home page</h1>)
+            }
+          } />
+
+
+          <Route path="/contact"  exact render={ 
+            () => {
+
+                return(<h1>Home page</h1>)
+            }
+          } />
+
+
+        </div>
+      </Router>
+
     );
   }
 }
